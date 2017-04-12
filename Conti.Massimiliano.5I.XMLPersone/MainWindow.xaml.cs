ab3ab478572904ad0535f9060e59d326e84780eb
MaxxiC <andmassic@icloud.com>
2017-04-12 08:17:02 +0200

    Aggiunta File

---
+++ Conti.Massimiliano.5I.XMLPersone/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Conti.Massimiliano._5I.XMLPersone
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                XElement dati = XElement.Load("Persone.xml");
+                IEnumerable<XElement> TutteLePersone = dati.Elements("Persona");
+
+                dgDati.ItemsSource = from item in dati.Elements("Persona") //TutteLePersone
+                                     select new Persona(item);
+
+            }
+            catch { MessageBox.Show("Errore"); }
+      
+        }
+    }
+}
---
+++ Conti.Massimiliano.5I.XMLPersone/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Conti.Massimiliano._5I.XMLPersone
+{
+    class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Indirizzo { get; set; }
+        public string NumeroDiTelefono { get; set; }
+
+        public Persona(XElement Uno)
+        {
+            Nome = Uno.Attribute("Nome").Value;
+            Cognome = Uno.Attribute("Cognome").Value;
+            Indirizzo = Uno.Attribute("Indirizzo").Value;
+            NumeroDiTelefono = Uno.Attribute("Numero").Value;
+        }
+    }
+}
